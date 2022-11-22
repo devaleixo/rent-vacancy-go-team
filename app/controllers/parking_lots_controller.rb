bc7725fa1d11ec6cf81d3603b877851ac85a1d70
Gabriel Aleixo <gabriel.aleixo061@gmail.com>
2022-11-22 13:35:31 -0300

    added authorization

--- app/controllers/parking_lots_controller.rb
+++ app/controllers/parking_lots_controller.rb
@@ -1,34 +1,39 @@
 class ParkingLotsController < ApplicationController
+  before_action :set_parkinglot, only: %i[show edit update destroy]
   def index
-    @parking_lots = ParkingLot.all
+    @parking_lots = policy_scope(ParkingLot)
   end
 
   def show
-    @parking_lot = ParkingLot.find(params[:id])
+    authorize @parking_lot
   end
 
   def new
     @parking_lot = ParkingLot.new
+    authorize @parking_lot
   end
 
   def create
     @parking_lot = ParkingLot.new(params_parkinglot)
+    @parking_lot.user = current_user
+    authorize @parking_lot
     @parking_lot.save
     redirect_to parking_lots_path
+
   end
 
   def edit
-    @parking_lot = ParkingLot.find(params[:id])
+    authorize @parking_lot
   end
 
   def update
-    @parking_lot = ParkingLot.find(params[:id])
+    authorize @parking_lot
     @parking_lot.update(params_parkinglot)
     redirect_to parking_lot_path(@parking_lot)
   end
 
   def destroy
-    @parking_lot = ParkingLot.find(params[:id])
+    authorize @parking_lot
     @parking_lot.destroy
     redirect_to parking_lots_index_path, status: :see_other
   end
@@ -40,6 +45,10 @@ class ParkingLotsController < ApplicationController
 private
 
 def params_parkinglot
+  params.require(:parking_lot).permit(:address, :price, :overview, :img_url)
 end
 
+def set_parkinglot
+  @parking_lot = ParkingLot.find(params[:id])
+end
 end
