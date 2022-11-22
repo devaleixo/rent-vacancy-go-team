4afdda8e38b0dcdf11e88fa8ce6a502a86757a92
Gabriel Aleixo <gabriel.aleixo061@gmail.com>
2022-11-22 20:57:46 -0300

    added reservation

---
+++ app/policies/reservation_policy.rb
@@ -0,0 +1,24 @@
+class ReservationPolicy < ApplicationPolicy
+  class Scope < Scope
+    # NOTE: Be explicit about which records you allow access to!
+    # def resolve
+    #   scope.all
+    # end
+  end
+
+  def create?
+    true
+  end
+
+  def new?
+    true
+  end
+
+  def show?
+    true
+  end
+
+  def destroy?
+    record.user == user
+  end
+end
