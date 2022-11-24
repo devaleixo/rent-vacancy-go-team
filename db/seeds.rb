45db8c93277d089aea3b8dcb4d540c91567b8ae3
Barbara Carloni <barbara-carloni@hotmail.com>
2022-11-24 14:25:53 -0300

    front index ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 puts "cleaning db"
+Reservation.destroy_all
 ParkingLot.destroy_all
 User.destroy_all
 puts "creating users"
