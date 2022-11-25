70a67f4e1cf595d891bc30354b349ec32fe8f9c6
Gabriel Aleixo <gabriel.aleixo061@gmail.com>
2022-11-25 11:15:53 -0300

    solving problems about end date

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,5 +1,5 @@
 class Reservation < ApplicationRecord
   belongs_to :parking_lot
   belongs_to :user
-  validates :reservation_date, :end_reservation_date, presence: true, uniqueness: { scope: :parking_lot }
+  validates :reservation_date, presence: true, uniqueness: { scope: :parking_lot }
 end
